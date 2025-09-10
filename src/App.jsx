53e67a5ecfce6d66c1839bc3b689907e8168983d
Nate M <codebynate@gmail.com>
2025-09-10 15:10:38 +0100

    Strip out default Vite styles and add starter content

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <h1>Weather Now</h1>
+            </header>
+            
+            <main>
+                Units
+
+                Switch to Imperial/Metric
+
+                Temperature
+
+                Celsius (°C)
+                Fahrenheit (°F)
+
+                Wind Speed
+
+                km/h
+                mph
+
+                Precipitation
+
+                Millimeters (mm)
+                Inches (in)
+
+                How's the sky looking today?
+
+                Search for a city, e.g., New York
+                Search
+
+                Feels like
+                {/* Insert temperature here */}
+
+                Humidity
+                {/* Insert humidity here */}
+
+                Wind
+                {/* Insert wind here */}
+
+                Precipitation
+                {/* Insert precipitation here */}
+
+                Daily forecast
+                {/* Insert daily forecast for the next 7 days here */}
+
+                Hourly forecast
+                {/* Insert hourly forecast for the selected day here */}
+
+            </main>
+
+            <footer>
+                <div class="attribution">
+                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer noopener">Frontend Mentor</a>.
+                    Built by <a href="https://nate-dev.com" target="_blank" rel="noreferrer noopener">Nate</a>.
+                </div>
+            </footer>
+        </>
+    )
 }
 
 export default App
