ced4fbf5faebf9dee4b5f5b66b4e35a6bb1ec668
Nate M <codebynate@gmail.com>
2025-09-16 11:47:06 +0100

    Create component for hourly forecast

---
+++ src/components/Hourly.jsx
@@ -0,0 +1,46 @@
+export default function Hourly(data) {
+    return (
+        <section className="col-span-1 row-span-3 p-6">
+            <div className="flex flex-nowrap justify-between">
+                <h3 className="text-style-5">Hourly forecast</h3>
+
+                <select name="day">
+                    <option value="monday">Monday</option>
+                    <option value="tuesday">Tuesday</option>
+                    <option value="wednesday">Wednesday</option>
+                    <option value="thursday">Thursday</option>
+                    <option value="friday">Friday</option>
+                    <option value="saturday">Saturday</option>
+                    <option value="sunday">Sunday</option>
+                </select>
+            </div>
+
+            <div>
+                <div className="flex flex-nowrap justify-between px-4 py-2.5">
+                    <div>icon 0 PM</div><div>00º</div>
+                </div>
+                <div className="flex flex-nowrap justify-between px-4 py-2.5">
+                    <div>icon 0 PM</div><div>00º</div>
+                </div>
+                <div className="flex flex-nowrap justify-between px-4 py-2.5">
+                    <div>icon 0 PM</div><div>00º</div>
+                </div>
+                <div className="flex flex-nowrap justify-between px-4 py-2.5">
+                    <div>icon 0 PM</div><div>00º</div>
+                </div>
+                <div className="flex flex-nowrap justify-between px-4 py-2.5">
+                    <div>icon 0 PM</div><div>00º</div>
+                </div>
+                <div className="flex flex-nowrap justify-between px-4 py-2.5">
+                    <div>icon 0 PM</div><div>00º</div>
+                </div>
+                <div className="flex flex-nowrap justify-between px-4 py-2.5">
+                    <div>icon 0 PM</div><div>00º</div>
+                </div>
+                <div className="flex flex-nowrap justify-between px-4 py-2.5">
+                    <div>icon 0 PM</div><div>00º</div>
+                </div>
+            </div>
+        </section>
+    )
+}
